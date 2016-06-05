1d8544ab066dd56ca956dc7ad74583c1bbe80f56
Ben Lloyd <ben@shobute.com>
2016-06-05 09:37:38 +0100

    Saved goban state to disk and restore on startup.

--- gobot.py
+++ gobot.py
@@ -5,78 +5,14 @@ from random import choice
 from sys import argv
 from collections import Counter
 from typing import List, Optional, Tuple
+from pickle import dump, load
 
 from slackclient import SlackClient
 from imgurpython import ImgurClient
 from PIL import Image, ImageDraw
 
+
 DEBUG = True
-
-
-class GoBot:
-    def __init__(self, token: str) -> None:
-        self.slack_client = SlackClient(token)
-        self.goban = Goban()
-        self.last_ran_crons = 0
-        self.last_ping = 0
-        self.channels = []
-
-    def start(self) -> None:
-        if self.slack_client.rtm_connect():
-            all_channels = self.slack_client.api_call('channels.list')['channels']
-            self.channels = [channel['id'] for channel in all_channels if channel['is_member']]
-            while True:
-                for event in self.slack_client.rtm_read():
-                    if 'type' in event and event['type'] == 'message' and 'text' in event and event['text'][0] == '!':
-                        self.process_command(event['text'], event['channel'], event['user'])
-
-                    if DEBUG:
-                        print(event)
-
-                self.hourly_crons()
-                self.ping()
-                sleep(1)
-        else:
-            print('Connection Failed, invalid token?')
-
-    def ping(self) -> None:
-        now = time()
-        if now > self.last_ping + 3:
-            self.slack_client.server.ping()
-            self.last_ping = now
-
-    def process_command(self, text: str, channel: str, user: str) -> None:
-        words = text.split()
-        command = words[0][1:].lower()
-        arguments = words[1:]
-
-        if command == 'vote':
-            if len(arguments) > 0:
-                result = self.goban.vote_move(arguments[0], user)
-            else:
-                result = "You need to type a move, e.g. `!vote J10`. I'm just a humble bot, not a mind reader."
-        elif command == 'votes':
-            result = self.goban.get_votes()
-        elif command == 'show':
-            result = self.goban.show_board()
-        else:
-            result = 'Invalid command, try: `!vote`, `!votes`, `!show`.'
-
-        self.slack_client.rtm_send_message(channel, result)
-
-    def hourly_crons(self) -> None:
-        now = time()
-
-        if DEBUG:
-            run_cron = now >= self.last_ran_crons + 10
-        else:
-            run_cron = now >= self.last_ran_crons + 60 * 60 and datetime.now().minute == 0
-
-        if run_cron:
-            result = self.goban.play_move()
-            if result:
-                self.slack_client.rtm_send_message(self.channels[0], result)
-            self.last_ran_crons = now
 
 
 class Goban:
@@ -241,6 +177,87 @@ class Goban:
         upload = self.imgur_client.upload_from_path(file_path)
 
         self.image_url = upload['link']
+
+
+class GoBot:
+    STATE_FILE_NAME = 'goban.pkl'
+
+    def __init__(self, token: str) -> None:
+        self.slack_client = SlackClient(token)
+        self.last_ran_crons = 0
+        self.last_ping = 0
+        self.channels = []
+        self.goban = self.load_goban()
+
+    def start(self) -> None:
+        if self.slack_client.rtm_connect():
+            all_channels = self.slack_client.api_call('channels.list')['channels']
+            self.channels = [channel['id'] for channel in all_channels if channel['is_member']]
+            while True:
+                for event in self.slack_client.rtm_read():
+                    if 'type' in event and event['type'] == 'message' and 'text' in event and event['text'][0] == '!':
+                        self.process_command(event['text'], event['channel'], event['user'])
+
+                    if DEBUG:
+                        print(event)
+
+                self.hourly_crons()
+                self.ping()
+                sleep(1)
+        else:
+            print('Connection Failed, invalid token?')
+
+    def ping(self) -> None:
+        now = time()
+        if now > self.last_ping + 3:
+            self.slack_client.server.ping()
+            self.last_ping = now
+
+    def process_command(self, text: str, channel: str, user: str) -> None:
+        words = text.split()
+        command = words[0][1:].lower()
+        arguments = words[1:]
+
+        if command == 'vote':
+            if len(arguments) > 0:
+                result = self.goban.vote_move(arguments[0], user)
+            else:
+                result = "You need to type a move, e.g. `!vote J10`. I'm just a humble bot, not a mind reader."
+        elif command == 'votes':
+            result = self.goban.get_votes()
+        elif command == 'show':
+            result = self.goban.show_board()
+        else:
+            result = 'Invalid command, try: `!vote`, `!votes`, `!show`.'
+
+        self.slack_client.rtm_send_message(channel, result)
+
+    def hourly_crons(self) -> None:
+        now = time()
+
+        if DEBUG:
+            run_cron = now >= self.last_ran_crons + 10
+        else:
+            run_cron = now >= self.last_ran_crons + 60 * 60 and datetime.now().minute == 0
+
+        if run_cron:
+            result = self.goban.play_move()
+            if result:
+                self.slack_client.rtm_send_message(self.channels[0], result)
+                self.save_goban()
+
+            self.last_ran_crons = now
+
+    def save_goban(self) -> None:
+        with open(self.STATE_FILE_NAME, 'wb') as file:
+            dump(self.goban, file)
+
+    def load_goban(self) -> Goban:
+        try:
+            with open(self.STATE_FILE_NAME, 'rb') as file:
+                return load(file)
+        except FileNotFoundError:
+            return Goban()
 
 
 if __name__ == '__main__':
